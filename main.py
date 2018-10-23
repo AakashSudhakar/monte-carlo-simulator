8a67a337b8d68eb4edfd94df08dda44ce2e78847
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-10-23 13:52:15 -0700

    Rewrote roll_dice() function with clearer print statements

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 FILENAME:               main.py
 AUTHOR:                 Aakash Sudhakar
 DATE CREATED:           Monday, October 22, 2018
-DATE LAST EDITED:       Monday, October 22, 2018
+DATE LAST EDITED:       Tuesday, October 23, 2018
 DESCRIPTION:            Main file containing logic for the Monte Carlo Python Simulation project
                         based off of the advanced Python project on pythonprogramming.net. 
 """
@@ -10,7 +10,17 @@ DESCRIPTION:            Main file containing logic for the Monte Carlo Python Si
 import math, random
 
 def roll_dice():
-    return random.randint(1, 100)
+    roll = random.randint(1, 100)
+
+    if roll == 100:
+        print("\nRoll is {}.\n\nYOU LOSE.\n".format(roll))
+        return False
+    elif roll <= 50:
+        print("\nRoll is {}, which is inclusively between 1 and 50.\n\nYOU LOSE.\n".format(roll))
+        return False
+    else:
+        print("\nRoll is {}, which is inclusively between 51 and 99.\n\nYOU WIN!\n".format(roll))
+        return True
 
 def main():
     x = 0
