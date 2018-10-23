969f798116aa42b650b5e26f007955b7fcb8f23a
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-10-23 14:07:50 -0700

    Created and cleaned up simple bettor function

--- main.py
+++ main.py
@@ -11,23 +11,40 @@ import math, random
 
 def roll_dice():
     roll = random.randint(1, 100)
-
     if roll == 100:
-        print("\nRoll is {}.\n\nYOU LOSE.\n".format(roll))
+        # print("\nRoll is {}.\n\nYOU LOSE.\n".format(roll))
         return False
     elif roll <= 50:
-        print("\nRoll is {}, which is inclusively between 1 and 50.\n\nYOU LOSE.\n".format(roll))
+        # print("\nRoll is {}, which is inclusively between 1 and 50.\n\nYOU LOSE.\n".format(roll))
         return False
     else:
-        print("\nRoll is {}, which is inclusively between 51 and 99.\n\nYOU WIN!\n".format(roll))
+        # print("\nRoll is {}, which is inclusively between 51 and 99.\n\nYOU WIN!\n".format(roll))
         return True
 
+def simple_bettor(funds, initial_wager, wager_count):
+    """
+    Main function to receive static monetary quantity and run simple betting algorithm. 
+    """
+    value, wager, current_wager = funds, initial_wager, 0
+    while current_wager < wager_count:
+        if roll_dice() is True:
+            value += wager
+        else:
+            value -= wager
+        current_wager += 1
+        # print("\nCURRENT FUNDS: ${}\n".format(value))
+    return value
+
 def main():
-    x = 0
-    while x < 100:
-        result = roll_dice()
-        print(result)
-        x += 1
+    (funds, initial_wager, wager_count) = (10000, 100, 100)
+    final_funds = simple_bettor(funds=funds, initial_wager=initial_wager, wager_count=wager_count)
+    print("\nSTARTING FUNDS:  ${}\nFINAL FUNDS:  ${}".format(funds, final_funds))
+    if final_funds > funds:
+        print("\nAfter {} wagers, you won ${}.\n\nCONGRATULATIONS!\n".format(wager_count, final_funds - funds))
+    elif final_funds == funds:
+        print("\nAfter {} wagers, you didn't win or lose any money.\n\nINTERESTING...\n".format(wager_count))
+    else:
+        print("\nAfter {} wagers, you lost ${}.\n\nEPIC FAIL!\n".format(wager_count, funds - final_funds))
 
 if __name__ == "__main__":
     main()
